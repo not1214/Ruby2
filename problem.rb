fd78ec9879391e27c1050a64ae52488890154ee2
not1214 <91748221+not1214@users.noreply.github.com>
2022-01-22 21:49:15 +0900

    アルゴリズム

--- problem.rb
+++ problem.rb
@@ -1,25 +1,50 @@
-n = gets.to_i
-cnt = {}
-n.times do |i|
-  cnt[i+1] = 0
+N = gets.to_i
+hash = {}
+N.times do |i|
+  hash[i+1] = 0
 end
  
-n.times do |i|
+N.times do |i|
   a = gets.to_i
-  cnt[a] += 1
+  hash[a] += 1
 end
  
-ans = [0,0]
-n.times do |i|
-  if cnt[i+1] == 0
-    ans[-1] = i + 1
-  elsif cnt[i+1] == 2
-    ans[0] = i + 1
+result = [0,0]
+N.times do |i|
+  if hash[i+1] == 0
+    result[1] = i + 1
+  elsif hash[i+1] == 2
+    result[0] = i + 1
   end
 end
  
-if ans == [-1,-1]
+if result == [0,0]
   puts 'Correct'
 else
-  puts ans.join(' ')
+  puts result.join(' ')
 end
+
+# N = gets.to_i
+# # puts N
+# # puts O
+# # A = arr[1..N].sort
+# # puts A
+# arr = []
+
+# result = [0,0]
+# for i in 1..N do
+#   arr[i-1] = gets.to_i
+# end
+# for i in 1..N do
+#   if arr.count(i) == 0
+#     result[1] += i
+#   elsif arr.count(i) == 2
+#     result[0] += i
+#   end
+# end
+
+# if result == [0,0]
+#   puts "Correct"
+# else 
+#   puts result.join(' ')
+# end
